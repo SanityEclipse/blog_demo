3c82323fa362815960fe062024307abbfef966cb
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2017-03-20 22:15:40 -0600

    Basic nav bar added.

---
+++ app/site-layout/nav-bar/nav-bar.component.js
@@ -0,0 +1,21 @@
+(function(){
+
+  'use strict';
+
+  angular.module('app')
+    .component('navBar', {
+      controller  :  controller,
+      templateUrl : 'app/site-layout/nav-bar/nav-bar.view.html',
+      css         : 'app/site-layout/nav-bar/nav-bar.style.css'
+    })
+
+  function controller() {
+    const vm = this;
+
+    vm.isNavCollapsed = true;
+    vm.isCollapsed = false;
+    vm.isCollapsedHorizontal = false;
+
+  }
+
+})();
