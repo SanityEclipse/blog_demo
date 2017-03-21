04a620491e11467c3a812e10b9c11a957eb76013
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2017-03-20 22:35:42 -0600

    Basic footer added.

---
+++ app/site-layout/bottom-footer/bottom-footer.component.js
@@ -0,0 +1,17 @@
+(function(){
+
+  'use strict';
+
+  angular.module('app')
+    .component('bottomFooter', {
+      controller  :  controller,
+      templateUrl : 'app/site-layout/bottom-footer/bottom-footer.view.html',
+      css         : 'app/site-layout/bottom-footer/bottom-footer.style.css'
+    })
+
+  function controller() {
+    const vm = this;
+
+  }
+
+})();
