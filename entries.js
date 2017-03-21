3dde06f1605eb7bed92bbad8392f32fe8c059e62
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2017-03-21 12:12:37 -0600

    Basic styling complete. Added entry service. Ready forexpress and node integration.

---
+++ entries.js
@@ -0,0 +1,30 @@
+var entries = [{
+  title: 'Entry 1',
+  image: 'http://placehold.it/125x125',
+  date: '01/01/2012',
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In finibus felis odio, id luctus velit pulvinar quis. Mauris eros turpis, convallis condimentum lorem quis, consequat gravida augue. Nulla ultrices pharetra enim, sed dignissim elit
+      aliquam sed. Quisque libero lacus, suscipit ac lectus in, placerat iaculis sem. Donec lorem dolor, maximus non scelerisque ac, eleifend at orci. Vivamus diam erat, condimentum et scelerisque ac, tincidunt vitae orci. Suspendisse condimentum
+      lorem lorem, et varius lorem viverra quis. Vestibulum purus dolor, ullamcorper sed dapibus quis, mollis pulvinar orci. Aliquam convallis rhoncus ipsum, nec bibendum sapien interdum vitae. Nullam tellus augue, mattis ut bibendum nec, egestas
+      quis sapien.',
+  comment: []
+
+},{
+  title: 'Entry 2',
+  image: 'http://placehold.it/125x125',
+  date: '02/18/1017',
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In finibus felis odio, id luctus velit pulvinar quis. Mauris eros turpis, convallis condimentum lorem quis, consequat gravida augue. Nulla ultrices pharetra enim, sed dignissim elit
+      aliquam sed. Quisque libero lacus, suscipit ac lectus in, placerat iaculis sem. Donec lorem dolor, maximus non scelerisque ac, eleifend at orci. Vivamus diam erat, condimentum et scelerisque ac, tincidunt vitae orci. Suspendisse condimentum
+      lorem lorem, et varius lorem viverra quis. Vestibulum purus dolor, ullamcorper sed dapibus quis, mollis pulvinar orci. Aliquam convallis rhoncus ipsum, nec bibendum sapien interdum vitae. Nullam tellus augue, mattis ut bibendum nec, egestas
+      quis sapien.',
+  comment: []
+
+},{
+  title: 'Entry 3',
+  image: 'http://placehold.it/125x125',
+  date: '10/31/2012',
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In finibus felis odio, id luctus velit pulvinar quis. Mauris eros turpis, convallis condimentum lorem quis, consequat gravida augue. Nulla ultrices pharetra enim, sed dignissim elit
+      aliquam sed. Quisque libero lacus, suscipit ac lectus in, placerat iaculis sem. Donec lorem dolor, maximus non scelerisque ac, eleifend at orci. Vivamus diam erat, condimentum et scelerisque ac, tincidunt vitae orci. Suspendisse condimentum
+      lorem lorem, et varius lorem viverra quis. Vestibulum purus dolor, ullamcorper sed dapibus quis, mollis pulvinar orci. Aliquam convallis rhoncus ipsum, nec bibendum sapien interdum vitae. Nullam tellus augue, mattis ut bibendum nec, egestas
+      quis sapien.',
+  comment: []
+}];
